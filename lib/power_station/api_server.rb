8804d5a0529d2ecdbc20ac4999c0220dd8ed56fb
Stephen Augenstein <perl.programmer@gmail.com>
2022-04-24 18:24:44 -0400

    Remove APIs for now - not compatible with new architecture

--- lib/power_station/api_server.rb
+++ lib/power_station/api_server.rb
@@ -4,8 +4,8 @@ require 'base64'
 
 module PowerStation
   class ApiServer
-    def initialize(mqtt_server, port)
-      @mqtt_server = mqtt_server
+    def initialize(event_bus, port)
+      @event_bus = event_bus
       @port = port
     end
 
@@ -36,80 +36,12 @@ module PowerStation
       )
 
       @server.mount_proc '/' do |req, res|
-        if req.path == '/api/v1/dump_memory'
-          handle_dump_memory(req, res)
-        elsif req.request_method == 'POST' && req.path == '/api/v1/update'
-          handle_update(req, res)
-        else
-          res.status = 404
-          res['Content-Type'] = 'text/plain; charset=utf-8'
-          res.body = 'Not Found'
-        end
+        res.status = 404
+        res['Content-Type'] = 'text/plain; charset=utf-8'
+        res.body = 'Not Found'
       end
 
       @server
-    end
-
-    def handle_dump_memory(req, res)
-      # Is the requested client connected?
-      client = lookup_client(req.query['serial_number'])
-      if client.nil?
-        res.status = 404
-        res['Content-Type'] = 'application/json; charset=utf-8'
-        res.body = '{"error":"Client Not Connected"}'
-        return
-      end
-
-      # Look up page
-      page =
-        case req.query['page'].to_i
-        when MqttClient::STATUS_PAGE_ID
-          client.status_page
-        when MqttClient::CONTROL_PAGE_ID
-          client.control_page
-        when MqttClient::WIFI_PAGE_ID
-          client.wifi_page
-        else
-          res.status = 404
-          res['Content-Type'] = 'application/json; charset=utf-8'
-          res.body = '{"error":"Unknown Page Type"}'
-          return
-        end
-
-      # Build response
-      res.status = 200
-      res['Content-Type'] = 'application/json; charset=utf-8'
-      res.body = {
-        page: req.query['page'].to_i,
-        serial_number: client.serial_number,
-        data: Base64.strict_encode64(page.raw)
-      }.to_json
-    end
-
-    def handle_update(req, res)
-      req_json = JSON.parse(req.body)
-
-      # Is the requested client connected?
-      client = lookup_client(req_json['serial_number'])
-      if client.nil?
-        res.status = 404
-        res['Content-Type'] = 'text/plain; charset=utf-8'
-        res.body = 'Client Not Connected'
-        return
-      end
-
-      # Make request
-      data = Base64.strict_decode64(req_json['data'])
-      client.make_request(req_json['page'], req_json['offset'], data)
-
-      # Build response
-      res.status = 200
-      res['Content-Type'] = 'text/plain; charset=utf-8'
-      res.body = 'Request Sent'
-    end
-
-    def lookup_client(serial_number)
-      @mqtt_server.clients.reverse.find { _1.serial_number == serial_number }
     end
   end
 end
